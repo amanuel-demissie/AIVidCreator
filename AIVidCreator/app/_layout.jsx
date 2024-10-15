30877dd482434c887ada6ab1ace95ebed30b7b25
Aman <amanuelan10@gmail.com>
2024-10-15 12:33:46 -0500

    set up tailwind and font

--- AIVidCreator/app/_layout.jsx
+++ AIVidCreator/app/_layout.jsx
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import {React, useEffect} from 'react'
+import { Stack, SplashScreen } from 'expo-router'
+import {useFonts} from 'expo-font';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+SplashScreen.preventAutoHideAsync(); //Makes the native splash screen (configured in app.json) remain visible until hideAsync is called.
+
+const RootLayout = () => {
+
+    const [fontsLoaded, error] = useFonts({
+        "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+        "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+        "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+        "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+        "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+        "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+        "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+        "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+        "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+      });
+
+      useEffect(() => {
+        if (error) throw error;
+    
+        if (fontsLoaded) {
+          SplashScreen.hideAsync();
+        }
+      }, [fontsLoaded, error]);
+    
+      if (!fontsLoaded) {
+        return null;
+      }
+    
+      if (!fontsLoaded && !error) {
+        return null;
+      }
+
+    return (
+        <Stack>
+            <Stack.Screen name="index" options={{headerShown: false}} />
+        </Stack>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default RootLayout
--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 
 4. Copy/paste App.jsx onto app/_layout.jsx, delete App.jsx
 5. In package.json change "main": "expo-router/entry", and add scheme
+
+Setting up NativeWind
+6. npm install nativewind (it's a universal style system for react-native)
+7. npm install --save-dev tailwindcss@3.3.2 
+9. Run npx tailwindcss init to create a tailwind.config.js file
+10. Set up fonts by editing tailwind.config file and babel.config.js
+11. Edit layout.jsx, pasting fonts, useFonts, etc ...
